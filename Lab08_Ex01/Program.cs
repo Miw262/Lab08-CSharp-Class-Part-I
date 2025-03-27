087d9fd7c9032f836d68386aaf5b76b9d2914fa3
Miw262 <chauat246@gmail.com>
2025-03-28 01:17:42 +0700

    1

---
+++ Lab08_Ex01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Person p = new Person();
+        System.Console.WriteLine($"Type of p is {p.GetType()}");
+    }
+}
+
+class Person
+{
+    // อาจมี properties หรือ methods ที่เกี่ยวข้อง
+}
---
+++ Lab08_Ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Person p = new Person();
+        System.Console.WriteLine($"Type of p is {p.GetType()}");
+    }
+}
+
+class Person
+{
+}
